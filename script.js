a5d7f977f81c5ef4e433a9829b878a1f31208b44
Connor <connorclifford17@gmail.com>
2019-11-05 15:32:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*window.addEventListener("load", expand());
+function expand(){
+  var coll = document.getElementById("button");
+    coll.addEventListener("click", function() {
+      var text = document.getElementById("text");
+      if (text.style.display === "block") {
+        text.style.display = "none";
+        }
+      else {
+        text.style.display = "block";
+        }
+    });
+  }
+*/
